b03f281e44488e1705e00b5e1c38dc490d6b9da3
Vincenzo Costantino <120407575+Vinci97@users.noreply.github.com>
2023-01-25 17:57:04 +0100

    esercizio 25-01-23

---
+++ 25-01-2023/script.js
@@ -0,0 +1,52 @@
+import myJson from "./mock.json" assert {type: "json"}
+const createEL = (type) => document.createElement(type);
+const queryS = (element)=> document.querySelector(element);
+const bodyEl = queryS("body");
+
+const cardEl = (data)=>{
+    const divCard = createEL("div");
+    divCard.className = "card";
+    const imgCnt = createEL("div");
+    imgCnt.className = "img-container";
+    const cont = createEL("div");
+    cont.className = "cont";
+
+    const gender = createEL("p");
+    gender.textContent = "gender: "+data.gender; 
+    const birthDate = createEL("p");
+    birthDate.textContent ="birthDate: "+ data.birthDate; 
+    const name =createEL("p");
+    name.textContent=data.firstName+" "+data.lastName;
+
+    const email = createEL("p");
+    email.textContent = data.email; 
+    const phone = createEL("p");
+    phone.textContent = data.phone; 
+
+    const image = createEL("img");
+    image.setAttribute('src',data.image);
+    image.setAttribute('alt','#');
+
+    const line = createEL("div");
+    line.className = "line";
+
+    const line2 = createEL("div");
+    line2.className = "line2";
+
+   
+
+   imgCnt.append(image,name,email,phone);
+   cont.append(gender,birthDate)
+   divCard.append(imgCnt,line,cont);
+    return divCard;
+}
+
+
+const button = createEL("button");
+button.className = "show";
+button.textContent="show";
+bodyEl.appendChild(button);
+button.addEventListener("click",()=>{
+    button.style.display= "none";
+    myJson.users.map(item=> bodyEl.appendChild(cardEl(item)))
+});
