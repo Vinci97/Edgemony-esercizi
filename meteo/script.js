410c36f3ec2bb27870c2acd46ba9bea427a05b51
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2023-04-27 02:13:51 +0200

    serch

--- meteo/script.js
+++ meteo/script.js
@@ -1,75 +1,56 @@
-import { meteoCardGen,qS } from "./utils.js";
+import { meteoCardGen, qS } from "./utils.js";
 const section = qS('section');
-const Button = qS('.myButton')
-const input = qS('.myImput')
-const citta = ["Roma,it,ISO+3166","Palermo","Potenza","Venezia","Aosta","Perugia","Trento","Firenze","Cagliari","Bari","Torino","Campobasso","Ancona","Genova","Milano","Trieste","Bologna","Napoli","Catanzaro","Aquila"];
+const searchForm = qS('#searchForm');
+const input = qS('.myInput');
+const citta = ["Roma,it,ISO+3166", "Palermo", "Potenza", "Venezia", "Aosta", "Perugia", "Trento", "Firenze", "Cagliari", "Bari", "Torino", "Campobasso", "Ancona", "Genova", "Milano", "Trieste", "Bologna", "Napoli", "Catanzaro", "Aquila"];
 citta.sort();
 let fetchCitta = [];
 let searchedValue = "";
 Promise.all(
-    citta.map(item =>fetch(`http://api.openweathermap.org/data/2.5/weather?q=${item}&lang=en&units=metric&appid=f3a68efaccb877c2e30750fefdd7d0f7`).then(res => res.json()
-))).then(data=>{
-    fetchCitta = data;
-    data.map(item => section.appendChild(meteoCardGen(item)));
-    input.addEventListener("keyup", ()=> {
-        if (input.value.length >= 3) {
-            Button.style.display = "";
-        } else {
-            Button.style.display = "none";
-        }
-      });
+  citta.map(item => fetch(`http://api.openweathermap.org/data/2.5/weather?q=${item}&lang=en&units=metric&appid=f3a68efaccb877c2e30750fefdd7d0f7`).then(res => res.json()))
+).then(data => {
+  fetchCitta = data;
+  data.map(item => section.appendChild(meteoCardGen(item)));
 
-    Button.addEventListener('click',(event)=>{
-        onPostsDelete()
-        searchedValue = event.target.value;
-       const newArrey= fetchCitta.filter((item) =>item.name.toLowerCase()===searchedValue.toLowerCase() )
-       newArrey.map(item=>section.appendChild(meteoCardGen(item)))
-        // {
-        //     if(item.name.toLowerCase()===searchedValue.toLowerCase()){
-        //             section.appendChild(meteoCardGen(item));
-        //     }
-        // })
-    })
-    // Button.addEventListener('click',()=>{
-    //     const filterText = input.value.toLowerCase();
-    //     const postsEls = document.querySelectorAll('.Container');
-        
-    //     postsEls.forEach((card)=> {
-    //       const cityName = card.qS('.name').textContent.toLowerCase();
-    //       if (cityName.includes(filterText)) {
-    //         card.style.display = "";
-    //       } else {
-    //         card.style.display = "none";
-    //       }
-    //     });
-       
-    // })
-    const Select = qS("#mySelect");
-
-fetchCitta.forEach(item => {
-  const option = document.createElement("option");
-  option.text = item.name;
-  Select.add(option);
-});
-
-Select.addEventListener("change", ()=> {
-  const selectedArrayName = Select.value;
-  const postsEls = document.querySelectorAll('.Container');
-  postsEls.forEach((card)=> {
-    if (card.textContent.includes(selectedArrayName)) {
-      card.style.display = "block";
+  input.addEventListener("keyup", () => {
+    if (input.value.length >= 3) {
+      searchForm.querySelector('.myButton').style.display = "";
     } else {
-      card.style.display = "none";
+      searchForm.querySelector('.myButton').style.display = "none";
     }
   });
+
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    onPostsDelete();
+    searchedValue = input.value;
+    const newArrey = fetchCitta.filter((item, index) => citta[index].split(',')[0].toLowerCase() === searchedValue.toLowerCase());
+    newArrey.map(item => section.appendChild(meteoCardGen(item)));
+  });
+
+  const Select = qS("#mySelect");
+
+  fetchCitta.forEach(item => {
+    const option = document.createElement("option");
+    option.text = item.name;
+    Select.add(option);
+  });
+
+  Select.addEventListener("change", () => {
+    const selectedArrayName = Select.value;
+    const postsEls = document.querySelectorAll('.Container');
+    postsEls.forEach((card) => {
+      if (card.textContent.includes(selectedArrayName)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+
 });
 
-})
-
-
-const onPostsDelete = ()=>{
-    const postsEls = document.querySelectorAll('.Container');
-    postsEls.forEach((post)=> post.remove())
-
+const onPostsDelete = () => {
+  const postsEls = document.querySelectorAll('.Container');
+  postsEls.forEach((post) => post.remove());
 }
-
